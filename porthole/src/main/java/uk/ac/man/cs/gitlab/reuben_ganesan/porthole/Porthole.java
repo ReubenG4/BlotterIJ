6f4306c8791a5051f154584b515893b5f3f6bfc9
Reuben Ganesan <reuben.ganesan@gmail.com>
2019-10-25 03:19:45 +0100

    Skeleton for ImageJ2 plugin started

--- porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/Porthole.java
+++ porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/Porthole.java
@@ -1,13 +1,24 @@
 package uk.ac.man.cs.gitlab.reuben_ganesan.porthole;
 
-/**
- * Hello world!
- *
- */
-public class Porthole 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+@Plugin(type = Command.class, headless = true,menuPath = "Porthole")
+public class Porthole implements Command{
+
+	//Ask context for access to services
+	@Parameter
+	private LogService logService;
+	
+	@Parameter
+	private UIService uiService;
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
