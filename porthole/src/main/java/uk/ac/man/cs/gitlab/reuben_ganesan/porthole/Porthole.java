a1a1ca6c9984699ffa34abf4e02040ff0eb24a9c
Reuben Ganesan <reuben.ganesan@gmail.com>
2019-10-25 02:48:40 +0100

    Initial Commit

---
+++ porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/Porthole.java
@@ -0,0 +1,13 @@
+package uk.ac.man.cs.gitlab.reuben_ganesan.porthole;
+
+/**
+ * Hello world!
+ *
+ */
+public class Porthole 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
