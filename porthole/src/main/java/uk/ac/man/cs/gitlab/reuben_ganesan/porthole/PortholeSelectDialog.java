78fb6e3088ea6d9e9e7a78d08cf26a6ea47fbdf3
Reuben Ganesan <reuben.ganesan@gmail.com>
2019-11-08 04:32:21 +0000

    Simpler initial file selection dialog with no listeners

--- porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/PortholeSelectDialog.java
+++ porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/PortholeSelectDialog.java
@@ -1,9 +1,16 @@
 package uk.ac.man.cs.gitlab.reuben_ganesan.porthole;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 
 import net.imagej.ops.OpService;
@@ -16,6 +23,7 @@ import org.scijava.ui.UIService;
 import java.awt.FlowLayout;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.awt.event.ActionEvent;
 
 /* Invoked by porthole command if images are to be selected */
@@ -33,7 +41,8 @@ public class PortholeSelectDialog extends JDialog implements ActionListener {
 	private ThreadService thread;
 	private UIService ui;
 	
-	private final JPanel contentPanel = new JPanel();
+	private JPanel buttonPanel = new JPanel();
+	private JPanel dataPanel = new JPanel();
 	final JFileChooser fc = new JFileChooser();
 
 	/**
@@ -54,10 +63,48 @@ public class PortholeSelectDialog extends JDialog implements ActionListener {
 	 * Create the dialog.
 	 */
 	public PortholeSelectDialog() {
+		setName("Porthole");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		buttonPanel.setLayout(new FlowLayout());
+		Object[] data = new Object[17];
+		File[] images = new File[17];
+		
+		
+		
+		
+		getContentPane().add(buttonPanel, BorderLayout.PAGE_START);
+		{
+			JToolBar fileBar = new JToolBar("");
+			
+			//File Button
+			JButton addButton = new JButton("Add");
+			fileBar.add(addButton);
+			
+			JButton removeButton = new JButton("Remove");
+			fileBar.add(removeButton);
+			
+			fileBar.setFloatable(false);
+	        fileBar.setRollover(true);		
+	        
+			buttonPanel.add(fileBar);
+		}
+		
+		getContentPane().add(dataPanel, BorderLayout.CENTER);
+		{
+			
+			JList imageList = new JList(data); 
+			imageList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			imageList.setLayoutOrientation(JList.VERTICAL);
+			imageList.setVisibleRowCount(-1);	
+			
+			JScrollPane listScroller = new JScrollPane(imageList);
+			listScroller.setPreferredSize(new Dimension(380, 200));
+			
+			dataPanel.add(listScroller);
+		}
+		
+		
 	}
 	
 	
