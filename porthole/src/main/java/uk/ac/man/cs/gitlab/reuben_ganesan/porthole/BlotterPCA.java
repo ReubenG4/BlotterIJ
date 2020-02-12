906fd125e6123d560ed73eb43ec83c57137b0846
Reuben Ganesan <reuben.ganesan@gmail.com>
2020-02-12 14:13:19 +0000

    Refactoring of extractPixelData. CovarianceData stub.

--- porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/BlotterPCA.java
+++ porthole/src/main/java/uk/ac/man/cs/gitlab/reuben_ganesan/porthole/BlotterPCA.java
@@ -16,69 +16,85 @@ import Jama.Matrix;
 public class BlotterPCA <T extends RealType<T> & NativeType<T>>extends BlotterFunction{
 	
 	
+	/* Main Function */
 	public Img<T> run(ArrayList<ImgWrapper<T>> inputData, Rectangle selection) {
+		
+		double[][][] pxlData = extractPixelData(inputData,selection);
+		
+		
+		
+		return null;
+		
+	}
+	
+	/* Extracts Pixel Data from selected region of input data */	
+	public double[][][] extractPixelData(ArrayList<ImgWrapper<T>> inputData, Rectangle selection) {
 		
 		//Initialise Interval 
 		FinalInterval region = FinalInterval.createMinSize(selection.x,selection.y,
 														   selection.width,selection.height);
-		
+				
 		//Initialise Iterator
 		Iterator<ImgWrapper<T>> itr = inputData.iterator();
-		
+
 		//Initialise bounds for target array to hold pixel data
 		int width = selection.width;
 		int height = selection.height;
-		
+
 		//Initialise target array to hold pixel data
 		double[][][] pxlData = new double[width][height][inputData.size()];
-		
+
 		//Iterate through inputData
 		int zIndex = 0;
 		while(itr.hasNext()) {
-			
+
 			//Retrieve image
 			ImgWrapper<T> curr = itr.next();
-			
+
 			//Initialise cursor for image, cursor iterates within specified region/interval
 			RandomAccess<T> cursor = curr.getImg().randomAccess(region);
-			
+
 			//Declare pointer to hold return object from cursor
 			T value;
-					
+
 			//Initialise initial values for cursor
 			int xIndex = 0;
 			int yIndex = 0;
-			
+
 			//Set cursor to first position
 			cursor.setPosition(0,0);
-	
+
 			//Iterate through y-axis of image
 			while(yIndex < height) {		
 				xIndex = 0;
-				
+
 				//Iterate through x-axis of image
 				while(xIndex < width) {
 					//Get value of pixel and store it in pxlData
 					value = cursor.get();
 					pxlData[xIndex][yIndex][zIndex] = value.getRealDouble();
-					
+
 					//Move cursor 1 pixel on x-axis
 					cursor.move(1,0);
 					xIndex++;
 				}//while x-axis end
-				
+
 				//move cursor 1 pixel on y-axis
 				cursor.move(1,1);
 				yIndex++;
 			}//while y-axis end
-			
+
 			//Update zIndex to point to next z of pxlData
 			zIndex++;
 			//Null currently accessed image pointer to allow for garbage collection
 			curr.nullImg();
 		}
 		
-		return null;
+		return pxlData;	
+	}
+	
+	
+	public class CovarianceData{
 		
 	}
 }
